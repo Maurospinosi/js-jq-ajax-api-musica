e1f5fc647ee83790738ee115119faa8204720f81
Mauro <mauro.spinosi94@gmail.com>
2020-09-17 16:33:48 +0200

    Esercizio completo (-bonus)

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
 $(document).ready(function() {
-	//Code
+  $.ajax(
+    {
+      "url": "https://flynn.boolean.careers/exercises/api/array/music",
+      "method": "GET",
+      "success": function (data, stato) {
+        var resp = data.response;
+        albumMusica(resp);
+      },
+
+      error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+    }
+  );
 });
-// https://flynn.boolean.careers/exercises/api/array/music link
+
+function albumMusica (resp){
+  var source = $("#album-template").html();
+  var template = Handlebars.compile(source);
+
+  for (var i = 0; i < resp.length; i++){
+    var context = {
+      "copertina" : resp[i].poster,
+      "titolo" : resp[i].title,
+      "autore" : resp[i].author,
+      "genere" : resp[i].genre,
+      "anno" : resp[i].year,
+    };
+
+
+    var html = template(context);
+    $(".appeso").append(html);
+  }
+}
